ac9033de90798f979b3ae04e030b7651a6509d18
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-02-06 21:15:19 -0500

    added assertions

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use std::error::Error;
 
 use rand::Rng;
 
-
 pub struct Config {
     pub plaintext: String,
 }
@@ -18,72 +17,71 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    //what could go wrong? 
+    //what could go wrong?
     //Not much. I just encrypt user input. Pretty simple.
     //And i generate the key. Pretty simple.
     //This kind of thinking definitely leads to errors. Neither of those are Actually simple..
-    
+
     let key = generate_key(config.plaintext.len());
 
-    println!("Encrypting \"{}\" with key: \n{:#0X?}", config.plaintext, key);
+    println!(
+        "Encrypting \"{:?}\" with key: \n{:#0X?}",
+        config.plaintext, key
+    );
     assert_eq!(key.len(), config.plaintext.len());
 
     let ciphertext = encrypt(&config.plaintext, &key);
     println!("Ciphertext: \n{:0X?}", ciphertext);
     assert_eq!(ciphertext.len(), config.plaintext.len());
-    
-    let ciphertext_str = vec_to_string(ciphertext);
 
-    let recovered_plaintext = encrypt(&ciphertext_str, &key);
-    println!("Testing cipher, decrypting with key. Plaintext recovered: \n{:?}", recovered_plaintext);
-    
+    let recovered_plaintext = encrypt(&ciphertext, &key);
+    println!(
+        "Testing cipher, decrypting with key. Plaintext recovered: \n{:?}",
+        recovered_plaintext
+    );
+
     Ok(())
 }
 
-fn generate_key(length: usize) -> Vec<u8> {
+fn generate_key(length: usize) -> String {
+    //make this a String
     println!("generateing key with length {}", length);
-    let mut key: Vec<u8> = vec![];
+    let mut key: Vec<u8> = Vec::new();
     for _i in 0..length {
         key.push(rand::thread_rng().gen());
     }
-    key
+    assert_eq!(key.len(), length);
+    vec_to_string(key)
 }
 
-fn encrypt(plaintext: &str, key: &Vec<u8>) -> Vec<u8> {
+fn encrypt(plaintext: &String, key: &String) -> String {
     let plaintext_bytes = plaintext.as_bytes();
-    assert_eq!(plaintext.len(), plaintext_bytes.len());
-    assert_eq!(plaintext.len(), key.len());
+    let key_bytes = key.as_bytes();
+
+    assert_eq!(plaintext_bytes.len(), key_bytes.len());
+
     let mut ciphertext: Vec<u8> = vec![];
-
-    if key.len() != plaintext_bytes.len() {
-        panic!("Key length must match plaintext length: key_len={}, plaintext_len={}", key.len(), plaintext_bytes.len());
-    }
-
     for i in 0..plaintext_bytes.len() {
-        if i >= key.len() {
-            panic!("Index out of bounds: i={}, key_len={}", i, key.len());
-        }
-        ciphertext.push(plaintext_bytes[i] ^ key[i]);
+        ciphertext.push(plaintext_bytes[i] ^ key_bytes[i]);
     }
 
-    ciphertext
+    vec_to_string(ciphertext)
 }
 
-fn vec_to_string(vec: Vec<u8>) -> String {
-    let mut string = String::with_capacity(vec.len());
-
+fn vec_to_string(vec: Vec<u8>) -> String { //what am i doing wrong :(
+    let mut string = String::new();
     for i in 0..vec.len() {
         string.push(vec[i] as char);
     }
-    assert_eq!(vec.len(), string.len());
+    assert_eq!(string.len(), vec.len());
     string
 }
 
-    //otp this thing
-    //encrypting with key:
-    //key
-    //ciphertext: 
-    //ciphertext
-    //cat foo.txt < otp
-    //cat foo.txt | otp > bar.txt
-    //not in my control, i think these will just work.
+//otp this thing
+//encrypting with key:
+//key
+//ciphertext:
+//ciphertext
+//cat foo.txt < otp
+//cat foo.txt | otp > bar.txt
+//not in my control, i think these will just work.
