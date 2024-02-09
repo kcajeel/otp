085f98caefb09e09b4e93c5c32528c0329b1d435
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-02-09 09:12:51 -0500

    added more error types

--- src/error.rs
+++ src/error.rs
@@ -1,21 +1,23 @@
-use std::{error::Error, fmt};
+use std::{error::Error, fmt::{self, write}};
 
 #[derive(Debug)]
 pub enum ArgumentError {
-    NotEnoughArguments,
+    InvalidArgumentNumber,
     InvalidArguments,
     TextNotASCII,
     InvalidArgumentLengths,
+    Unfinished,
 }
 impl Error for ArgumentError {}
 
 impl fmt::Display for ArgumentError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            ArgumentError::NotEnoughArguments => write!(f, "Error: Not enough arguments."),
-            ArgumentError::InvalidArguments => write!(f, "Error: Invalid arguments."),
+            ArgumentError::InvalidArgumentNumber => write!(f, "Error: Invalid number of arguments. Use \"-h\" or \"--help\" for usage information."),
+            ArgumentError::InvalidArguments => write!(f, "Error: Invalid arguments. Use \"-h\" or \"--help\" for usage information."),
             ArgumentError::TextNotASCII => write!(f, "Error: Non-ASCII text. This program currently only supports ASCII encoded text."),
-            ArgumentError::InvalidArgumentLengths => write!(f, "Error: Key is shorter than the Ciphertext."),            
+            ArgumentError::InvalidArgumentLengths => write!(f, "Error: Key is shorter than the Ciphertext."),
+            ArgumentError::Unfinished => write!(f, "This part is unfinished ;)"),     
         }
     }
 }
