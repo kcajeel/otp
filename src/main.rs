9b3c7bde9edb03514de6d6c65a98e6f63c497812
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-02-09 09:13:16 -0500

    renamed Config to Program

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
+use otp::Program;
 use std::{env, process};
-use otp::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let program = Program::build(&args).unwrap_or_else(|err| {
         eprintln!("Problem passing arguments: {err}");
         process::exit(1);
     });
 
-    if let Err(e) = otp::run(config) {
+    if let Err(e) = otp::run(program) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
