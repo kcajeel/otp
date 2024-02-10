7ef81b3c6f9c7069f393150487886c9609543aaf
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-02-09 21:15:13 -0500

    added tests, worked on decryption

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,8 @@ impl TryFrom<&[String]> for Mode {
                     Err(ArgumentError::InvalidArguments)
                 }
             },
-            "-d" | "--decrypt" => { Err(ArgumentError::Unfinished)
+            "-d" | "--decrypt" => {
+                Err(ArgumentError::Unfinished)
                 // if Self::args_are_valid(value, 4)? {
                 //     if value[2].len() == value[3].len() {
                 //         Ok(Mode::Decrypt {
@@ -96,69 +97,128 @@ pub fn run(program: Program) -> Result<(), Box<dyn Error>> {
     }
 }
 
-fn run_encryption(plaintext: &str) {
+fn run_encryption(plaintext: &String) {
     let key = generate_key(plaintext.len());
 
     println!(
         "Encrypting \"{}\" with key: \n\"{}\"",
         plaintext,
-        vec_to_string(&key)
+        string_to_blocks(&key)
     );
-
     debug_assert_eq!(key.len(), plaintext.len());
 
-    let ciphertext = encrypt(&plaintext, &key);
-    println!("Ciphertext: \n\"{}\"", &ciphertext);
+    let ciphertext = encrypt(plaintext, &key);
     debug_assert_eq!(ciphertext.len(), plaintext.len());
+    println!("Ciphertext: \n\"{}\"", string_to_blocks(&ciphertext));
+
+    let recovered_plaintext = encrypt(&ciphertext, &key);
+    println!(
+        "Testing cipher, decrypting with key. Plaintext recovered: \n{:?}",
+        recovered_plaintext
+    );
 }
 
-fn run_decryption(ciphertext: &str, key: &str) {
+fn run_decryption(ciphertext: &String, key: &String) {
     println!("Decrypting \"{}\" with key \"{}\"", ciphertext, key);
 
-    debug_assert_eq!(ciphertext.len(), key.len());
+    debug_assert_eq!(ciphertext.len(), key.len(), "cipher len: {}, key len: {}", ciphertext.len(), key.len());
 
-    let plaintext = encrypt(ciphertext, key.as_bytes());
+    let plaintext = encrypt(&blocks_to_string(ciphertext), &blocks_to_string(key));
     debug_assert_eq!(plaintext.len(), ciphertext.len());
 
     println!("Plaintext: \"{}\"", plaintext);
 }
 
-fn generate_key(length: usize) -> Vec<u8> {
+fn generate_key(length: usize) -> String {
     const ASCII_RANGE: RangeInclusive<u8> = 32..=126; //range of printable ASCII characters
 
     println!("Generating key with length {}", length);
     let key: Vec<u8> = (0..length)
         .map(|_| rand::thread_rng().gen_range(ASCII_RANGE))
         .collect();
-    assert_eq!(key.len(), length);
-    key
+    debug_assert_eq!(key.len(), length);
+
+    vec_to_string(&key)
 }
 
-fn encrypt(plaintext: &str, key: &[u8]) -> String {
+fn encrypt(plaintext: &String, key: &String) -> String {
     let plaintext_bytes = plaintext.as_bytes();
-    assert_eq!(plaintext_bytes.len(), key.len());
+    debug_assert_eq!(plaintext_bytes.len(), key.len());
+    let key_bytes = key.as_bytes();
+    debug_assert_eq!(plaintext_bytes.len(), key_bytes.len());
 
     let ciphertext: Vec<u8> = plaintext_bytes
         .iter()
-        .zip(key)
-        .map(|(x, y)| x ^ y)
+        .zip(key_bytes)
+        .map(|(x, y)| (x ^ y))
         .collect();
 
-    assert_eq!(ciphertext.len(), plaintext_bytes.len());
+    debug_assert_eq!(ciphertext.len(), plaintext_bytes.len());
     vec_to_string(&ciphertext)
 }
 
 fn vec_to_string(vec: &Vec<u8>) -> String {
-    let string = String::from_utf8_lossy(&vec).to_string();
-    assert_eq!(
-        string.len(),
-        vec.len(),
-        "string: {}, vec: {:?}",
-        string,
-        vec
-    );
+    let mut string = String::new();
+    for i in 0..vec.len() {
+        string.push(vec[i] as char);
+    }
+    assert_eq!(string.len(), vec.len());
     string
 }
+
+fn string_to_blocks(string: &String) -> String {
+    let bytes = string.as_bytes();
+    let mut blocks = String::new();
+    
+    for byte in bytes {
+        for i in 0..8 {
+            if byte & 1 << i != 0 {
+                blocks.push('█');
+            } else {
+                blocks.push(' ');
+            }
+        }
+    }
+    blocks
+}
+
+fn blocks_to_string(blocks: &String) -> String {
+    let bits = blocks_to_bits(blocks.to_string());
+    let mut bytes: Vec<u8> = vec![];
+
+    for byte in bits.chunks(8) {
+        let mut count = 0;
+        if byte.len() % 8 == 0 {
+            for i in 0..8 {
+                if byte[i] == 1 {
+                    count += 2_u8.pow(i.try_into().unwrap());
+                }
+            }
+            bytes.push(count);
+        }
+    }
+    
+    let string = String::from_utf8_lossy(&bytes);
+    string.into_owned()
+}
+
+fn blocks_to_bits(mut blocks: String) -> Vec<u8> {
+    let mut bytes: Vec<u8> = vec![];
+    
+    for _i in 0..blocks.len() {
+        if let Some(block) = blocks.pop() {
+            // println!("\"{}\"", block);
+            if block == '█' {
+                bytes.push(1);
+            } else {
+                bytes.push(0);
+            }
+        }
+    }
+    bytes.reverse();
+    bytes
+}
+
 
 #[cfg(test)]
 mod test {
@@ -172,5 +232,13 @@ mod test {
 
         let recovered_plaintext = encrypt(&ciphertext, &key);
         assert_eq!(recovered_plaintext, plaintext);
+    }
+
+    #[test]
+    fn test_decryption() {
+        let plaintext = "testing".to_string();
+        let key = generate_key(plaintext.len());
+        let ciphertext = encrypt(&plaintext, &key);
+        assert_eq!(encrypt(&ciphertext, &key), plaintext);
     }
 }
