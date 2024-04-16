17a7af921737b83b98cbfd15605ff5d855295928
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2024-04-16 10:10:42 -0400

    cleaned up error messages

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let program = Program::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem passing arguments: {err}");
+        eprintln!("{err}");
         process::exit(1);
     });
 
     if let Err(e) = otp::run(program) {
-        eprintln!("Application error: {e}");
+        eprintln!("{e}");
         process::exit(1);
     }
 }
