9835df7ea90e9502aed8f306eb8a7baa3f7fa778
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2025-02-14 16:56:13 -0500

    Removed unneeded error types

--- src/error.rs
+++ src/error.rs
@@ -4,9 +4,7 @@ use std::{error::Error, fmt};
 pub enum ArgumentError {
     InvalidArgumentNumber,
     InvalidArguments,
-    TextNotASCII,
     InvalidArgumentLengths,
-    UnsupportedDecryptionArguments,
 }
 impl Error for ArgumentError {}
 
@@ -15,9 +13,7 @@ impl fmt::Display for ArgumentError {
         match self {
             ArgumentError::InvalidArgumentNumber => write!(f, "Error: Invalid number of arguments. Use \"-h\" or \"--help\" for usage information."),
             ArgumentError::InvalidArguments => write!(f, "Error: Invalid arguments. Use \"-h\" or \"--help\" for usage information."),
-            ArgumentError::TextNotASCII => write!(f, "Error: Non-ASCII text. This program currently only supports ASCII encoded text."),
             ArgumentError::InvalidArgumentLengths => write!(f, "Error: Key is shorter than the Ciphertext."),
-            ArgumentError::UnsupportedDecryptionArguments => write!(f, "Error: Please decrypt with the \"█/ \" format"),
         }
     }
 }
